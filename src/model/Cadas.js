a732b67c641d50032570ed4c6b820c4648614008
Marcelo Oliveira <marcelo_alfahot@hotmail.com>
2020-06-07 20:54:18 -0300

    Ajust backend

--- src/model/Cadas.js
+++ src/model/Cadas.js
@@ -1,9 +1,23 @@
 const mongoose = require("mongoose");
 
 const CadasSchema = new mongoose.Schema({
-  name: String,
-  phone: String,
-  age: Number,
+  name: {
+    type: String,
+    required: true,
+    maxlength: 255,
+  },
+  phone: {
+    type: Number,
+    required: true,
+    maxlength: 9,
+    minlength: 8,
+  },
+  age: {
+    type: Number,
+    required: true,
+    minlength: 1,
+    maxlength: 3,
+  },
 });
 
 module.exports = mongoose.model("Cadastro", CadasSchema);
